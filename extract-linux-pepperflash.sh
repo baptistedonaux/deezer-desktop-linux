a503608acb580322682c55a01b402adfb028a43d
Baptiste Donaux <baptiste.donaux@gmail.com>
2017-01-06 14:51:40 +0100

    [FIX] pepperflash extraction

--- extract-linux-pepperflash.sh
+++ extract-linux-pepperflash.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 available () {
   command -v $1 >/dev/null 2>&1
@@ -9,29 +9,15 @@ if available wget; then
   SILENT_DL="wget -qO-"
   LOUD_DL="wget"
 elif available curl; then
-  SILENT_DL="curl -s"
+  SILENT_DL="curl -sL"
   LOUD_DL="curl -O"
 else
   echo "Install wget or curl" >&2
   exit 1
 fi
 
-# Check which Chrome stream the user selected to fetch from
-CHROME_STREAM=stable
-while [ 0 ]; do
-  if [ "$1" = "-u" -o "$1" = "--unstable" ]; then
-    CHROME_STREAM=unstable
-    shift 1
-  elif [ "$1" = "-b" -o "$1" = "--beta" ]; then
-    CHROME_STREAM=beta
-    shift 1
-  else
-    break
-  fi
-done
-
 # Set Output dir
-PPAPI_FLASH_INSTALL_DIR=${PPAPI_FLASH_INSTALL_DIR:-/opt/google/chrome/PepperFlash}
+PPAPI_FLASH_INSTALL_DIR=${PPAPI_FLASH_INSTALL_DIR:-/usr/lib/adobe-flashplugin}
 
 # Set temp dir
 TMP=${TMP:-/tmp}
@@ -40,22 +26,31 @@ TMP=${TMP:-/tmp}
 STAGINGDIR=$TMP/pepper-flash-staging
 
 # Setup Arch
-ARCH=$(uname -m | sed 's/i.86/i386/')
+case $(uname -m) in
+  x86_64) ARCH=x86_64 ;;
+    i?86) ARCH=i386 ;;
+esac
 
-# Work out the latest stable Google Chrome if VERSION is unset
-VERSION=$($SILENT_DL https://dl.google.com/linux/direct/google-chrome-${CHROME_STREAM}_current_$ARCH.rpm | head -c96 | strings | rev | awk -F"[:-]" '/emorhc/ { print $2 }' | rev)
+# Work out the VERSION
+VERSION=$($SILENT_DL http://www.adobe.com/software/flash/about/ | grep -FA1 'Chromium-based browsers - PPAPI' | grep -Eo '([0-9]+\.){3}[0-9]+' | tail -n1)
 
 # Error out if $VERISON is unset, e.g. because previous command failed
-if [ -z $VERSION ]; then
+if [ -z "$VERSION" ]; then
   echo "Could not work out the latest version; exiting" >&2
   exit 1
 fi
 
 # Don't start repackaging if the same version is already installed
-if [ -e "$PPAPI_FLASH_INSTALL_DIR/chrome-version-$VERSION" ] ; then
-  echo "The latest Flash is already installed"
-  exit 0
+if [ -r "$PPAPI_FLASH_INSTALL_DIR/manifest.json" ] ; then
+  CUR_VER=$(grep -Eo '"version" *: *"([0-9]+\.){3}[0-9]+",' "$PPAPI_FLASH_INSTALL_DIR/manifest.json" | cut -d'"' -f 4)
+  if [ "$CUR_VER" = "$VERSION" ]; then
+    echo "The latest Flash ($VERSION) is already installed"
+    exit 0
+  fi
 fi
+
+# Now we could screw things up so exit on first error
+set -e
 
 # If the staging directory is already present from the past, clear it down
 # and re-create it.
@@ -63,43 +58,26 @@ if [ -d "$STAGINGDIR" ]; then
   rm -fr "$STAGINGDIR"
 fi
 
-set -e
-mkdir -p "$STAGINGDIR"
+mkdir -p "$STAGINGDIR$PPAPI_FLASH_INSTALL_DIR"
 cd "$STAGINGDIR"
 
-# Now get the rpm
-$LOUD_DL https://dl.google.com/linux/direct/google-chrome-${CHROME_STREAM}_current_${ARCH}.rpm
-
-DOWNLOADVERSION=$(head -c96 "google-chrome-${CHROME_STREAM}_current_${ARCH}.rpm" | strings | rev | awk -F"[:-]" '/emorhc/ { print $2 }' | rev)
-
-if [ ! "$VERSION" = "$DOWNLOADVERSION" ]; then
-  echo "The version downloaded ($DOWNLOADVERSION) is different from the version expected ($VERSION)" >&2
-  exit 1
-fi
+# Now get the tarball
+$LOUD_DL "http://fpdownload.adobe.com/pub/flashplayer/pdc/$VERSION/flash_player_ppapi_linux.${ARCH}.tar.gz"
 
 # Extract the contents of the Google Chrome binary package
-RPMHDRLGTH=$(LANG=C grep -abom1 7zXZ google-chrome-${CHROME_STREAM}_current_${ARCH}.rpm)
-
-echo "Extracting Flash from the Chrome RPM ..."
-tail -c+${RPMHDRLGTH%:*} google-chrome-${CHROME_STREAM}_current_${ARCH}.rpm | xz -d | cpio --quiet -id "./opt/google/chrome*/PepperFlash/*"
-
-cd opt/google/chrome*/PepperFlash
-
-# Add version number file
-touch "chrome-version-$VERSION"
+tar xf flash_player_ppapi_linux.${ARCH}.tar.gz -C "$STAGINGDIR$PPAPI_FLASH_INSTALL_DIR"
+chmod -R u+w,go+r-w,a-s .
 
 # Escalate privileges if needed and copy files into place
 if [ "$UID" = 0 ]; then
-  mkdir -p "$PPAPI_FLASH_INSTALL_DIR"
-  cp * "$PPAPI_FLASH_INSTALL_DIR"
+  tar --owner=0 --group=0 -cf- ".$PPAPI_FLASH_INSTALL_DIR" | tar -xf- -C /
 elif [ -r /etc/os-release ] && grep -qx 'ID=\(ubuntu\|linuxmint\)' /etc/os-release; then
-  echo "Calling sudo ... If prompted, please enter your password so Flash can be copied into place"
-  sudo mkdir -p "$PPAPI_FLASH_INSTALL_DIR"
-  sudo cp * "$PPAPI_FLASH_INSTALL_DIR"
+  echo "Calling sudo ... (if prompted, please enter your password, so Flash can be copied into place)"
+  tar --owner=0 --group=0 -cf- ".$PPAPI_FLASH_INSTALL_DIR" | sudo tar -xf- -C /
 else
   echo "Please enter your root password so Pepper Flash can be copied into place"
-  su -c "sh -c \"mkdir -p $PPAPI_FLASH_INSTALL_DIR && cp * $PPAPI_FLASH_INSTALL_DIR\""
+  su -c "sh -c \"tar --owner=0 --group=0 -cf- .$PPAPI_FLASH_INSTALL_DIR | tar -xf- -C /\""
 fi
 
 # Tell the user we are done
-echo "Flash installed into $PPAPI_FLASH_INSTALL_DIR"
+printf "\nFlash installed into $PPAPI_FLASH_INSTALL_DIR\n"
