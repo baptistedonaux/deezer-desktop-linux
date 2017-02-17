a0cbfcae6b3b17572ad0d218b80e063f69f779a9
Baptiste Donaux <baptiste.donaux@gmail.com>
2017-02-17 20:55:12 +0100

    [FIX] can be built and runned

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
+# Requirement
+
+* pepperflashplugin-nonfree
+> [Debian](https://wiki.debian.org/PepperFlashPlayer/Installing)
+> [Ubuntu](http://packages.ubuntu.com/fr/yakkety/pepperflashplugin-nonfree)
+
 # How to run
 
 ```
 npm install
-npm run pepper
 npm start
 ```
 
 # How to build
 
-> /!\ Currently this part doesn't work.
-
 ```
 npm install
 cd app && npm install && cd ..
-npm run pepper
 npm run build
 npm run dist
 ```
--- app/main.js
+++ app/main.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const electron = require('electron');
-const app = electron.app;
-const fs = require('fs');
-const path = require("path");
-const Player = require("./player.js");
-const proc = require("process");
-
-const configPath = proc.env.HOME + "/.config/Deezer-desktop-linux";
+const electron = require('electron'),
+    app = electron.app,
+    fs = require('fs'),
+    path = require("path"),
+    Player = require("./player.js"),
+    proc = require("process"),
+    configPath = proc.env.HOME + "/.config/Deezer-desktop-linux";
 
 let mainWindow;
 
@@ -17,30 +16,25 @@ try {
     fs.mkdirSync(configPath, 755);
 }
 
-app.commandLine.appendSwitch('ppapi-flash-path', path.resolve(__dirname, 'plugins/libpepflashplayer.so'));
-app.commandLine.appendSwitch('ppapi-flash-version', JSON.parse(require("fs").readFileSync(path.resolve(__dirname, "plugins/manifest.json")).toString()).version);
+app.commandLine.appendSwitch('ppapi-flash-path', "/usr/lib/pepperflashplugin-nonfree/libpepflashplayer.so");
 
-app.on('ready', create);
+// app.commandLine.appendSwitch('ppapi-flash-path', path.resolve(__dirname, 'plugins/libpepflashplayer.so'));
+// app.commandLine.appendSwitch('ppapi-flash-version', JSON.parse(require("fs").readFileSync(path.resolve(__dirname, "plugins/manifest.json")).toString()).version);
+
 app.on('ready', function() {
+    create();
+
     new Player(mainWindow, configPath);
 });
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', function () {
-    if (mainWindow === null) {
-        create();
-    }
+    app.quit();
 });
 
 function create() {
     mainWindow = new electron.BrowserWindow({
         "icon": path.resolve(__dirname, "resources/dz-client-linux-x128.png"),
-        "web-preferences": {
+        "webPreferences": {
             'plugins': true
         }
     });
